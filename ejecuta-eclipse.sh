0d5c71c6fd248a93a4b3383039d1a38ecfa6f03f
Jaime Chavarriaga <jaime.chavarriaga@gmail.com>
2023-11-06 12:11:38 +0000

    Agrega URL en el shell que ejecuta eclipse

--- ejecuta-eclipse.sh
+++ ejecuta-eclipse.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Run Eclipse using the FeatureIDE perspective
+
+# Eclipe command line options
+# - see: https://help.eclipse.org/latest/index.jsp?topic=%2Forg.eclipse.platform.doc.isv%2Freference%2Fmisc%2Fruntime-options.html
 
 BASEDIR=$(dirname "$0")
 
